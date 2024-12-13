e9506c3a839713aa148c65a0b2e48d370792c09b
Isabella Pereira <isabella.pereira@unico.io>
2024-12-13 13:18:44 -0300

    primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-        Olá mundo da bells!!
+        teste do Olá mundo da bells!!
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
